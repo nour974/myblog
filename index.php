46bf7a3dfa4e04768a4ff9765f842dff8e5eb8a6
nour974 <96582494+nour974@users.noreply.github.com>
2021-12-26 23:31:36 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Nour Blog</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css" />
+
+    <style>
+
+    </style>
+</head>
+
+<body>
+    <header id="navbar">
+        <?php include 'nav.php' ?>
+
+        <!-- Articles -->
+
+        <div class="header">
+            <h2>Welcome to my blog</h2>
+        </div>
+
+        <div class="row">
+            <div class="leftcolumn">
+                <div class="card">
+                    <h2>TITLE HEADING</h2>
+                    <h5>Title description, Dec 7, 2017</h5>
+                    <img class="fakeimg" src="assets/images/dev.jpg" alt="" srcset="">
+                    <p>Some text..</p>
+                    <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+                </div>
+            </div>
+            <div class="rightcolumn">
+                <div class="card">
+                    <h3>Popular Post</h3>
+                    <img class="fakeimg" src="assets/images/dev.jpg" alt="" srcset="">
+                    <br>
+                    <img class="fakeimg" src="assets/images/studying.jpg" alt="" srcset="">
+                    <br>
+                    <img class="fakeimg" src="assets/images/article.jpg" alt="" srcset="">
+                </div>
+                <div class="card">
+                    <h3>Follow Me</h3>
+                    <p>Some text..</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="footer">
+            <h2>Footer</h2>
+        </div>
+
+        <!-- Articles -->
+
+    </header>
+    <script src="assets/js/index.js"></script>
+</body>
+
+</html>
